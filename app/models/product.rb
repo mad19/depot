8e35f1f2fd64fc7b425c5fdfa2f574d98214b043
mad19 <cross657@mail.ru>
2015-03-19 16:24:56 +0300

    Upd 3.1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ActiveRecord::Base
+  has_many :line_items, dependent: :nullify
   validates :name, presence: true, length: { maximum: 200 }
   validates :price, presence: true, numericality: {greater_than: 0}
   validates :weight, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank: true
+
+  before_destroy :can_destroy?
+
+  def can_destroy?
+    line_items.blank?
+  end
 end
